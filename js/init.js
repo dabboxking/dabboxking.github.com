f8099ae38f15f9e6ab9ae2444b79c76a5e93050d
Emmanuel J. Reyme <reyme3@gmail.com>
2015-12-13 14:15:35 -0500

    more changes

---
+++ js/init.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300
+    });
+
+    //$('.scrollspy').scrollSpy();
+
+    // $.ajax({
+    //     type : 'GET',
+    //     url  : 'quotes.json',
+    //     success : function(data) {
+    //         $('#author').text("-" + data[0].author);
+    //         $('#message').text(data[0].message);
+    //     }
+    // });
+}); // end of document ready
