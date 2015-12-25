4ae397311895123605526fb8a3c263fde5262c0e
Emmanuel J. Reyme <reyme3@gmail.com>
2015-12-25 15:56:25 -0500

    i tred

--- js/init.js
+++ js/init.js
@@ -6,12 +6,4 @@ $(document).ready(function(){
 
     //$('.scrollspy').scrollSpy();
 
-    // $.ajax({
-    //     type : 'GET',
-    //     url  : 'quotes.json',
-    //     success : function(data) {
-    //         $('#author').text("-" + data[0].author);
-    //         $('#message').text(data[0].message);
-    //     }
-    // });
 }); // end of document ready
