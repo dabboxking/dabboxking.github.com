10480bdaefb99ce295ca79c0d049af498fdb3323
Emmanuel J. Reyme <reyme3@gmail.com>
2016-06-28 00:30:44 -0400

    random quotes

--- js/init.js
+++ js/init.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
       menuWidth: 300
     });
 
+    var url = 'http://quote-api.cfapps.io/quotes';
+    $.get(url,function(data) {
+        var rand = data[Math.floor(Math.random() * data.length)];
+        var quote = data[rand];
+        $('#author').text(quote.author);
+        $('#message').text(quote.content);
+    });
+
     //$('.scrollspy').scrollSpy();
 
 }); // end of document ready
