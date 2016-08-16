8539b25c6cea79bd1683adfd8efb90cd24f3e421
Emmanuel J. Reyme <reyme3@gmail.com>
2016-08-15 21:20:53 -0400

    fix

--- js/init.js
+++ js/init.js
@@ -15,7 +15,7 @@ $(function(){
 
     var getRandomQuote = function() {
       //var quote = quotes[Math.floor(Math.random() * quotes.length)];
-      var url = 'https://the-quote-api.herokuapp.com/random';
+      var url = 'https://the-quote-api.herokuapp.com/quotes/random';
       //var url = 'http://localhost:8080/quotes/random';
       $.getJSON(url,function(quote) {
         $('#author').text('- '+quote.name);
